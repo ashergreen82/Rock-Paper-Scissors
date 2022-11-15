53a1e39c5dcb598c5b14eef4deb370978cca4a6b
Ashergreen82 <greena@rogers.com>
2022-11-15 06:24:07 -0500

    Cleaned up the code and added some comments

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-// document.addEventListener("click", function (event) {
-//     console.log(event);
-// })
-
-let rock_was_clicked;
-let paper_was_clicked;
-let scissors_was_clicked;
+// My event lisetners, one for each image.  The mouse hover was best done using CSS instead of trying to use the event listener.
 const rock_was_selected = document.getElementById("rock");
-// rock_was_selected.addEventListener("mouseover", function (e) {
-//     console.log(e.type)
-//     console.log(e);
-// });
 rock_was_selected.addEventListener("click", game_logic);
+
 const paper_was_selected = document.getElementById("paper");
 paper_was_selected.addEventListener("click", game_logic);
 
 const scissors_was_selected = document.getElementById("scissors");
 scissors_was_selected.addEventListener("click", game_logic);
 
+// And the game logic is over here.
 function game_logic(element) {
     console.log("Game Logic Executed");
     console.log(`${element.target.id} was clicked`);
