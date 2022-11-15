0de3c674a212fd0d453b43b7f2efdf16095e6b90
Ashergreen82 <greena@rogers.com>
2022-11-15 05:22:02 -0500

    Added hovering routine that grows the images

--- index.js
+++ index.js
@@ -6,23 +6,26 @@ let rock_was_clicked;
 let paper_was_clicked;
 let scissors_was_clicked;
 const rock_was_selected = document.getElementById("rock");
-rock_was_selected.addEventListener("mouseover", function (e) {
-    console.log(e.type)
-    console.log(e);
-});
+// rock_was_selected.addEventListener("mouseover", function (e) {
+//     console.log(e.type)
+//     console.log(e);
+// });
 rock_was_selected.addEventListener("click", function (e) {
-    rock_was_clicked = e.target.innerHTML;
-    console.log(e.type);
-    console.log(e);
-    console.log(rock_was_selected);
-    console.log(rock_was_clicked);
+    // rock_was_clicked = e.target.innerHTML;
+    test(e.target.id);
+    console.log("clicked");
+    // console.log(e.type);
+    // console.log(e);
+    // console.log(rock_was_selected);
+    // console.log(rock_was_clicked);
 });
 const paper_was_selected = document.getElementById("paper");
-paper_was_selected.addEventListener("mouseover", function (e) {
-    console.log(e.type)
-    console.log(e);
-});
+// paper_was_selected.addEventListener("mouseover", function (e) {
+//     console.log(e.type)
+//     console.log(e);
+// });
 paper_was_selected.addEventListener("click", function (e) {
+    test(e.target.id);
     paper_was_clicked = e.target.innerHTML;
     console.log(e.type);
     console.log(e);
@@ -30,11 +33,12 @@ paper_was_selected.addEventListener("click", function (e) {
     console.log(paper_was_clicked);
 });
 const scissors_was_selected = document.getElementById("scissors");
-scissors_was_selected.addEventListener("mouseover", function (e) {
-    console.log(e.type)
-    console.log(e);
-});
+// scissors_was_selected.addEventListener("mouseover", function (e) {
+//     console.log(e.type)
+//     console.log(e);
+// });
 scissors_was_selected.addEventListener("click", function (e) {
+    test(e.target.id);
     scissors_was_clicked = e.target.innerHTML;
     console.log(e.type);
     console.log(e);
@@ -42,7 +46,7 @@ scissors_was_selected.addEventListener("click", function (e) {
     console.log(scissors_was_clicked);
 });
 
-function test() {
-    console.log(rock_was_clicked);
-    document.getElementById("test").innerHTML = "A Rock was selected";
+function test(element) {
+    console.log(`${element} was clicked`);
+    // document.getElementById("test").innerHTML = "A Rock was selected";
 };
